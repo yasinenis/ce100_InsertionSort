8404484447c29149abf7f9c5b99b68806bb1fa53
yasinenis <yasin.enis.tr>
2024-02-20 01:01:12 +0300

    🦗 something changed

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,31 +1,32 @@
 #include <stdio.h>
+using namespace std;
 
-void InsertionSort(int* A, int n);
+void InsertionSort(int* Array, int n);
 
 int main()
 {
-	int A[6] = { 5,2,4,6,1,3 };
-	int n = sizeof(A) / sizeof(int);
-	InsertionSort(A, n);
+	int Array[6] = {5 , 4 ,1 ,6 ,2 ,3};
+	int n = sizeof(Array) / sizeof(int);
+	InsertionSort(Array, n);
 	for (int i = 0; i < n; i++)
 	{
-		printf("%d", A[i]);
+		printf("%d", Array[i]);
 	}
 }
 
-void InsertionSort(int* A, int n)
+void InsertionSort(int* Array, int n)
 {
 	int key = 0;
 	int i = 0;
 	for (int j = 1; j < n; j++)
 	{
-		key = A[j];
+		key = Array[j];
 		i = j - 1;
-		while ((i > -1) && (A[i] > key))
+		while ((i > -1) && (Array[i] > key))
 		{
-			A[i + 1] = A[i];
+			Array[i + 1] = Array[i];
 			i = i - 1;
 		}
-		A[i + 1] = key;
+		Array[i + 1] = key;
 	};
 }
